07e967124da519b5760cfae7bf4435e6ac3779d3
Timothy Gu <timothygu@stanford.edu>
2022-05-23 23:10:27 -0700

    Working local cache

--- dnsserver/dnsserver.go
+++ dnsserver/dnsserver.go
@@ -31,266 +31,81 @@ package main
 */
 
 import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"net"
-	"os"
-	"strings"
+	"sync"
+
+	"github.com/miekg/dns"
+	log "github.com/sirupsen/logrus"
 )
 
-// DNSHeader describes the request/response DNS header
-type DNSHeader struct {
-	TransactionID  uint16
-	Flags          uint16
-	NumQuestions   uint16
-	NumAnswers     uint16
-	NumAuthorities uint16
-	NumAdditionals uint16
-}
-
-// DNSResourceRecord describes individual records in the request and response of the DNS payload body
-type DNSResourceRecord struct {
-	DomainName         string
-	Type               uint16
-	Class              uint16
-	TimeToLive         uint32
-	ResourceDataLength uint16
-	ResourceData       []byte
-}
-
-// Type and Class values for DNSResourceRecord
-const (
-	TypeA                  uint16 = 1 // a host address
-	ClassINET              uint16 = 1 // the Internet
-	FlagResponse           uint16 = 1 << 15
-	UDPMaxMessageSizeBytes uint   = 512 // RFC1035
-)
-
-// Pretend to look up values in a database
-func dbLookup(queryResourceRecord DNSResourceRecord) ([]DNSResourceRecord, []DNSResourceRecord, []DNSResourceRecord) {
-	var answerResourceRecords = make([]DNSResourceRecord, 0)
-	var authorityResourceRecords = make([]DNSResourceRecord, 0)
-	var additionalResourceRecords = make([]DNSResourceRecord, 0)
-
-	names, err := GetNames()
-	if err != nil {
-		return answerResourceRecords, authorityResourceRecords, additionalResourceRecords
-	}
-
-	if queryResourceRecord.Type != TypeA || queryResourceRecord.Class != ClassINET {
-		return answerResourceRecords, authorityResourceRecords, additionalResourceRecords
-	}
-
-	for _, name := range names {
-		if strings.Contains(queryResourceRecord.DomainName, name.Name) {
-			fmt.Println(queryResourceRecord.DomainName, "resolved to", name.Address)
-			answerResourceRecords = append(answerResourceRecords, DNSResourceRecord{
-				DomainName:         name.Name,
-				Type:               TypeA,
-				Class:              ClassINET,
-				TimeToLive:         31337,
-				ResourceData:       name.Address[12:16], // ipv4 address
-				ResourceDataLength: 4,
-			})
-		}
-	}
-
-	return answerResourceRecords, authorityResourceRecords, additionalResourceRecords
-}
-
-// RFC1035: "Domain names in messages are expressed in terms of a sequence
-// of labels. Each label is represented as a one octet length field followed
-// by that number of octets.  Since every domain name ends with the null label
-// of the root, a domain name is terminated by a length byte of zero."
-func readDomainName(requestBuffer *bytes.Buffer) (string, error) {
-	var domainName string
-
-	b, err := requestBuffer.ReadByte()
-
-	for ; b != 0 && err == nil; b, err = requestBuffer.ReadByte() {
-		labelLength := int(b)
-		labelBytes := requestBuffer.Next(labelLength)
-		labelName := string(labelBytes)
-
-		if len(domainName) == 0 {
-			domainName = labelName
-		} else {
-			domainName += "." + labelName
-		}
-	}
-
-	return domainName, err
-}
-
-// RFC1035: "Domain names in messages are expressed in terms of a sequence
-// of labels. Each label is represented as a one octet length field followed
-// by that number of octets.  Since every domain name ends with the null label
-// of the root, a domain name is terminated by a length byte of zero."
-func writeDomainName(responseBuffer *bytes.Buffer, domainName string) error {
-	labels := strings.Split(domainName, ".")
-
-	for _, label := range labels {
-		labelLength := len(label)
-		labelBytes := []byte(label)
-
-		responseBuffer.WriteByte(byte(labelLength))
-		responseBuffer.Write(labelBytes)
-	}
-
-	err := responseBuffer.WriteByte(byte(0))
-
-	return err
-}
-
-func handleDNSClient(requestBytes []byte, serverConn *net.UDPConn, clientAddr *net.UDPAddr) {
-	/**
-	 * read request
-	 */
-	var requestBuffer = bytes.NewBuffer(requestBytes)
-	var queryHeader DNSHeader
-	var queryResourceRecords []DNSResourceRecord
-
-	err := binary.Read(requestBuffer, binary.BigEndian, &queryHeader) // network byte order is big endian
-
-	if err != nil {
-		fmt.Println("Error decoding header: ", err.Error())
-	}
-
-	queryResourceRecords = make([]DNSResourceRecord, queryHeader.NumQuestions)
-
-	for idx, _ := range queryResourceRecords {
-		queryResourceRecords[idx].DomainName, err = readDomainName(requestBuffer)
-
-		if err != nil {
-			fmt.Println("Error decoding label: ", err.Error())
-		}
-
-		queryResourceRecords[idx].Type = binary.BigEndian.Uint16(requestBuffer.Next(2))
-		queryResourceRecords[idx].Class = binary.BigEndian.Uint16(requestBuffer.Next(2))
-	}
+func handle(rw dns.ResponseWriter, queryMsg *dns.Msg) {
+	log.Infof("received request from %v", rw.RemoteAddr())
 
 	/**
 	 * lookup values
 	 */
-	var answerResourceRecords = make([]DNSResourceRecord, 0)
-	var authorityResourceRecords = make([]DNSResourceRecord, 0)
-	var additionalResourceRecords = make([]DNSResourceRecord, 0)
+	output := make(chan TypedResourceRecord)
 
-	for _, queryResourceRecord := range queryResourceRecords {
-		newAnswerRR, newAuthorityRR, newAdditionalRR := dbLookup(queryResourceRecord)
+	// Look up queries in parallel.
+	go func(output chan<- TypedResourceRecord) {
+		var wg sync.WaitGroup
+		for _, query := range queryMsg.Question {
+			wg.Add(1)
+			go Lookup(&wg, query, output)
+		}
+		wg.Wait()
+		close(output)
+	}(output)
 
-		answerResourceRecords = append(answerResourceRecords, newAnswerRR...) // three dots cause the two lists to be concatenated
-		authorityResourceRecords = append(authorityResourceRecords, newAuthorityRR...)
-		additionalResourceRecords = append(additionalResourceRecords, newAdditionalRR...)
+	// Collect output from each query.
+	var answerResourceRecords []dns.RR
+	var authorityResourceRecords []dns.RR
+	var additionalResourceRecords []dns.RR
+	for rec := range output {
+		switch rec.Type {
+		case ResourceAnswer:
+			answerResourceRecords = append(answerResourceRecords, rec.Record)
+		case ResourceAuthority:
+			authorityResourceRecords = append(authorityResourceRecords, rec.Record)
+		case ResourceAdditional:
+			additionalResourceRecords = append(additionalResourceRecords, rec.Record)
+		}
 	}
 
 	/**
 	 * write response
 	 */
-	var responseBuffer = new(bytes.Buffer)
-	var responseHeader DNSHeader
-
-	responseHeader = DNSHeader{
-		TransactionID:  queryHeader.TransactionID,
-		Flags:          FlagResponse,
-		NumQuestions:   queryHeader.NumQuestions,
-		NumAnswers:     uint16(len(answerResourceRecords)),
-		NumAuthorities: uint16(len(authorityResourceRecords)),
-		NumAdditionals: uint16(len(additionalResourceRecords)),
-	}
-
-	err = Write(responseBuffer, &responseHeader)
-
-	if err != nil {
-		fmt.Println("Error writing to buffer: ", err.Error())
-	}
-
-	for _, queryResourceRecord := range queryResourceRecords {
-		err = writeDomainName(responseBuffer, queryResourceRecord.DomainName)
-
-		if err != nil {
-			fmt.Println("Error writing to buffer: ", err.Error())
-		}
-
-		Write(responseBuffer, queryResourceRecord.Type)
-		Write(responseBuffer, queryResourceRecord.Class)
-	}
+	response := new(dns.Msg)
+	response.SetReply(queryMsg)
+	response.RecursionAvailable = true
 
 	for _, answerResourceRecord := range answerResourceRecords {
-		err = writeDomainName(responseBuffer, answerResourceRecord.DomainName)
-
-		if err != nil {
-			fmt.Println("Error writing to buffer: ", err.Error())
-		}
-
-		Write(responseBuffer, answerResourceRecord.Type)
-		Write(responseBuffer, answerResourceRecord.Class)
-		Write(responseBuffer, answerResourceRecord.TimeToLive)
-		Write(responseBuffer, answerResourceRecord.ResourceDataLength)
-		Write(responseBuffer, answerResourceRecord.ResourceData)
+		response.Answer = append(response.Answer, answerResourceRecord)
 	}
 
 	for _, authorityResourceRecord := range authorityResourceRecords {
-		err = writeDomainName(responseBuffer, authorityResourceRecord.DomainName)
-
-		if err != nil {
-			fmt.Println("Error writing to buffer: ", err.Error())
-		}
-
-		Write(responseBuffer, authorityResourceRecord.Type)
-		Write(responseBuffer, authorityResourceRecord.Class)
-		Write(responseBuffer, authorityResourceRecord.TimeToLive)
-		Write(responseBuffer, authorityResourceRecord.ResourceDataLength)
-		Write(responseBuffer, authorityResourceRecord.ResourceData)
+		response.Ns = append(response.Ns, authorityResourceRecord)
 	}
 
 	for _, additionalResourceRecord := range additionalResourceRecords {
-		err = writeDomainName(responseBuffer, additionalResourceRecord.DomainName)
-
-		if err != nil {
-			fmt.Println("Error writing to buffer: ", err.Error())
-		}
-
-		Write(responseBuffer, additionalResourceRecord.Type)
-		Write(responseBuffer, additionalResourceRecord.Class)
-		Write(responseBuffer, additionalResourceRecord.TimeToLive)
-		Write(responseBuffer, additionalResourceRecord.ResourceDataLength)
-		Write(responseBuffer, additionalResourceRecord.ResourceData)
+		response.Extra = append(response.Extra, additionalResourceRecord)
 	}
 
-	serverConn.WriteToUDP(responseBuffer.Bytes(), clientAddr)
+	err := rw.WriteMsg(response)
+	if err != nil {
+		log.Errorf("error writing response %v", err)
+	}
 }
 
 func main() {
-	serverAddr, err := net.ResolveUDPAddr("udp", ":1053")
+	InitDB()
 
-	if err != nil {
-		fmt.Println("Error resolving UDP address: ", err.Error())
-		os.Exit(1)
+	s := &dns.Server{
+		Addr:    ":1053",
+		Net:     "udp",
+		Handler: dns.HandlerFunc(handle),
 	}
-
-	serverConn, err := net.ListenUDP("udp", serverAddr)
-
+	err := s.ListenAndServe()
 	if err != nil {
-		fmt.Println("Error listening: ", err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Println("Listening at: ", serverAddr)
-
-	defer serverConn.Close()
-
-	for {
-		requestBytes := make([]byte, UDPMaxMessageSizeBytes)
-
-		_, clientAddr, err := serverConn.ReadFromUDP(requestBytes)
-
-		if err != nil {
-			fmt.Println("Error receiving: ", err.Error())
-		} else {
-			fmt.Println("Received request from ", clientAddr)
-			go handleDNSClient(requestBytes, serverConn, clientAddr) // array is value type (call-by-value), i.e. copied
-		}
+		log.Fatalf("Error resolving UDP address: %v", err)
 	}
 }
